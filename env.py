6a5dc42ca8c4f64307171fc01134a8813e47392b
NaMlex <alexn.alexn@gmail.com>
2025-06-09 16:11:34 +0200

    add DATABASE_URL and SECRET_KEY

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_A09MOuRPKWoc@ep-proud-night-a2bmsfon.eu-central-1.aws.neon.tech/next_decal_apple_303478")
+
+os.environ.setdefault("SECRET_KEY", "jCn8K8jG6NK3apYfk8OhPqeeb/afKXqf2vQYRGyKxMITqS3JpToH+hZTB1juzjTw")    
--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2&3a^wv@zr$l(zy+hp60n6elj11n$*fbz2=n&pt@*g-&!(ohq8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
